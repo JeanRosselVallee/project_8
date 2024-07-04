aac16a84a55fe93ee5a48b246028ba8386abb482
JeanRosselVallee <35218552+JeanRosselVallee@users.noreply.github.com>
2024-07-04 23:06:50 +0200

    Streamlit simple_app

---
+++ simple_app.py
@@ -0,0 +1,59 @@
+'''
+simple_app.py
+Streamlit app
+'''
+
+# Init
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Load data
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data								# STREAM: Cache Function's Results
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+
+# Text
+st_print1 = st.text('Loading data...')		# STREAM: print Text
+data = load_data(10000)                 	# Load 10,000 rows
+st_print1.text('Loading data...done!')		
+
+# Condition
+if st.checkbox('Show raw data'):			# STREAM: input Checkbox
+	
+# Table
+	st.subheader('Raw data')				# STREAM: print Title
+	st.write(data)							# STREAM: print Pandas
+
+# Histogram
+st.subheader('Number of pickups by hour')	
+hist_values = np.histogram(					# Create Histogram
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0] 
+st.bar_chart(hist_values)					# STREAM: plot Graph
+
+# Slicer
+hour_to_filter = st.slider('hour', 0, 23, 17)  # (min, max, default)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+# Map
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)						# STREAM: plot Map
+
+
+
+
+# Main
+'''
+st.title('simple_app')
+number = st.slider("Pick a number", 0, 100)
+st.write(f"You selected: {number}")
+'''
