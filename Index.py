a1adc7b6f3080384a5375305de710c6284644ffe
JeanRosselVallee <35218552+JeanRosselVallee@users.noreply.github.com>
2024-07-05 10:02:47 +0200

    Update Index.py

--- Index.py
+++ Index.py
@@ -6,9 +6,35 @@ Main page of Streamlit app
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
+	page_title="Hello",
+	page_icon="👋",
+	layout="wide"	
 )
+
+# Customize the sidebar
+st.sidebar.title("About")
+st.sidebar.info("""
+	Web App URL: <https://project8-ocr.streamlit.app/>
+	GitHub Repository: <https://github.com/JeanRosselVallee/project_8>
+	""")
+st.sidebar.image("https://www.whenthebanksaysno.co.uk/wp-content/uploads/2023/05/D9585792-ED4C-4363-900E-1EDCE31B99B1.jpeg")
+
+# Customize page title
+st.title("Credit Application Dashboard")
+
+st.markdown(
+    """
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    """
+)
+
+st.header("Instructions")
+
+markdown = """
+1. Select a client's application for credit
+2. Find your favorite emoji from https://emojipedia.org.
+4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+"""
 
 st.write("# Welcome to Streamlit! 👋")
 
@@ -23,3 +49,4 @@ st.markdown(
 )
 
 
+st.markdown(markdown)
